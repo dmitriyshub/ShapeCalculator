960e7cf86d58a200ffa6284748803eff412cce48
dmitriyshub <d.shtranvasser@gmail.com>
2022-09-07 23:42:44 +0300

    Daily

---
+++ oop_ex/cards.py
@@ -0,0 +1,59 @@
+import random
+
+
+class Card:
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+
+    def show(self):
+        print('{} of {}'.format(self.value, self.suit))
+
+
+card = Card('Card', 6)
+card.show()
+
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for shape in ['Spades', 'Clubs', 'Diamonds', 'Hearts']:
+            for card in ['A','2','3','4','5','6','7','8','9','10','J','Q','K']:
+                self.cards.append(Card(shape, card))
+
+    def show(self):
+        for card in self.cards:
+            card.show()
+
+    def shuffle(self):
+        for i in range(len(self.cards)-1,0,-1):
+            r = random.randint(0,i)
+            self.cards[i] , self.cards[r] = self.cards[r] , self.cards[i]
+
+    def draw(self):
+
+        return self.cards.pop()
+
+
+
+deck = Deck()
+deck.shuffle()
+deck.show()
+print("------")
+card = deck.draw()
+card.show()
+print("------")
+card = deck.draw()
+card.show()
+print("------")
+card = deck.draw()
+card.show()
+print("------")
+card = deck.draw()
+card.show()
+print("------")
+deck.show()
--- oop_ex/main.py
+++ oop_ex/main.py
@@ -1,3 +1,4 @@
+import random
 class Circle:
   """
   Create a Circle class.
@@ -10,10 +11,48 @@ class Circle:
       self.radius = radius
       self.pi = 3.14
 
-  def get_area(self):
+  def __str__(self):
+      return f'The area of this {self.__class__.__name__} is {self.get_area()}\n' \
+             f'The radius of this {self.__class__.__name__} is {self.get_perimiter()}'
 
-      area = (self.pi * self.radius ** 2)
-      return f"The area is: {area}"
+  def get_area(self):
+      self.area = (self.pi * self.radius ** 2)
+      return self.area
+
+  def get_perimiter(self):
+      self.perimiter = 2 * self.pi*self.radius
+      return self.perimiter
 
 a= Circle(10)
+print(a)
 print(a.get_area())
+print(a.get_perimiter())
+
+class Square:
+    """
+  Create a Square class.
+- A Square class gets the length of a side.
+- Set a constructor, get and set functions of that side.
+- Write a function that calculates it area.
+- Write a function that calculates it perimeter.
+    """
+    def __init__(self,length):
+        self.length=length
+
+    def __str__(self):
+        return f'The area of this {self.__class__.__name__} is {self.get_area()}\n' \
+               f'The perimiter of this {self.__class__.__name__} is {self.get_perimiter()}'
+
+    def get_area(self):
+        self.area = self.length * self.length
+        return self.area
+
+    def get_perimiter(self):
+        self.perimiter = 4 * self.area
+        return self.perimiter
+
+b= Square(10)
+print(b)
+print(b.get_area())
+print(b.get_perimiter())
+
